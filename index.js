9037de11fed6816532839c91bd448f930ee3603d
Eric van Riet Paap <ericvrp@gmail.com>
2017-10-06 22:53:55 +0200

    ots of small experiments. No idea why getAccountData doesn't return the transfers. We need this to check for incoming messages.

--- index.js
+++ index.js
@@ -25,76 +25,70 @@ const IOTAP = require('iotap')           // https://github.com/jimthedev/iotap
 //   })
 // }
 
-
-const iota  = new IOTA({ host: 'http://node02.iotatoken.nl', port: 14265 })
+// const host = 'http://iota.bitfinex.com'; port = 80;
+const host = 'http://node02.iotatoken.nl'; port = 14265;
+// const host = 'http://localhost';           port = 15000;
+const iota  = new IOTA({ host: host, port: port })
+// const iota  = new IOTA({ host: 'http://node02.iotatoken.nl', port: 14265 })
 const iotap = IOTAP.create(iota) // promisified iota library
 
 
 //
-const seed = "BQIYTZINTTX9ZDVMUHI9Z9SOPQSDPSQQPPBGMELFGSXFVLPJRNZDONYGPCS9BTQBRPKEBWETJLRANJAKL";
-const seedEncoded = iota.utils.toTrytes(seed);
+const seed = 'BQIYTZINTTX9ZDVMUHI9Z9SOPQSDPSQQPPBGMELFGSXFVLPJRNZDONYGPCS9BTQBRPKEBWETJLRANJAKL'
+const seedEncoded = iota.utils.toTrytes(seed)
 
-const securitylevel = 1;
-const nAddresses = 10;
+const destinationAddress = '99VUCXYVWSWHKMH9SUAOGN9TXWLOAKGCTSJHXM9WLJGN9HOXEKITUZFAVZJEDYWAGOATJSIHPHKSCIOXBGOGU9CPKX'
+
+// const securitylevel = 2; // 2 is default
+const nAddresses = 25;
 
 
 function getAccountData() {
   console.log('> getAccountData')
   const options = {
     start: 0,
-    end: nAddresses-1,
+    end: nAddresses,
     // security: securitylevel,
     threshold: 0
   }
-  iotap.getAccountData(seedEncoded, options).then(data=>{
+  iotap.getAccountData(seedEncoded, options).then(data => {
+  // iotap.getAccountData(seedEncoded).then(data => {
     console.log('--------- getAccountData data');
-    console.log(JSON.stringify(data));
+    console.log(JSON.stringify(data, null, 4)); // XXX why is transfers[] always empty?
     // console.log(data.addresses)
 
-    iotap.getBalances(data.addresses, 100).then(json => {
-      console.log('--------- getAccountData -> getBalances json');
-      console.log(json)
-    })
+    // iotap.getBalances(data.addresses, 100).then(json => {
+    //   console.log('--------- getAccountData -> getBalances json');
+    //   console.log(json)
+    // })
   });
 }
 
 
-function getNewAddress() {
-  console.log('> getNewAddress')
-  const options = {
-    // checksum: false,
-    total: nAddresses,
-    // security: securitylevel,
-    returnAll: true
-  }
-  iotap.getNewAddress(seedEncoded, options).then(publicAddresses => {
-    console.log('--------- getNewAddress publicAddresses');
-    console.log(publicAddresses);
+// function getNewAddress() {
+//   console.log('> getNewAddress')
+//   const options = {
+//     // checksum: false,
+//     total: nAddresses,
+//     // security: securitylevel,
+//     returnAll: true
+//   }
+//   iotap.getNewAddress(seedEncoded, options).then(publicAddresses => {
+//     console.log('--------- getNewAddress publicAddresses');
+//     console.log(publicAddresses);
+//
+//     iotap.getBalances(publicAddresses, 100).then(json => {
+//       console.log('--------- getNewAddress -> getBalances json');
+//       console.log(json)
+//     })
+//   });
+// }
 
-    iotap.getBalances(publicAddresses, 100).then(json => {
-      console.log('--------- getNewAddress -> getBalances json');
-      console.log(json)
-    })
-  });
-}
 
-
-function getBalances() {
-  console.log('> getBalances')
-  const publicAddresses = [
-    'SJMBZORRFQYBVNIBFETF9LBVKYNAIFFIKHCJRYYIPKIIFEYJIIM9WZLLLGGLRRACXMEXUDAGIKJBFOIN9SHUHJI9YW',
-    'WDEYHTHCDMX9CQHUDMDAVJYZDQWGKODWOJVCFHDSDDCUOCRM9SRWVLBZSWHVHZHDOHSJPJYULTAOXXLECKEQHWFVFC',
-    'M9PGQUOSOKRSIXMBVNBNQVXDWVKAIIRLZUNCIFVWMYBUJUVSHFMQLRUSH9URSCG9OQ9FRRRITCSUPRVRXG9SMU9JXC',
-  ]
-  iotap.getBalances(publicAddresses, 100).then(json => {
-    let totalBalance = 0
-    const balances = []
-    for (const balance of json.balances) {
-      const b = parseInt(balance, 10)
-      totalBalance += b
-      balances.push(b)
-    }
-    console.log('--------- getBalances totalBalance', totalBalance, balances)
+function getNodeInfo() {
+  iotap.getNodeInfo().then(data => {
+    console.log('--------- getNodeInfo data')
+    console.log(JSON.stringify(data, null, 4))
   })
 }
 
@@ -106,7 +100,48 @@ function getInputs() {
   })
 }
 
-getInputs()
-// getAccountData()
+
+function sendTransfer() {
+  const messageToSend = {
+      'id': 'JHKSDIUFSDFKJSDFUI',
+      'message': new Date().toString()
+  }
+  const messageStringified = JSON.stringify(messageToSend)
+  const messageTrytes = iota.utils.toTrytes(messageStringified)
+  // console.log('message', messageTrytes)
+
+  // here we define the transfers object, each entry is an individual transaction
+  const transfer = [{
+      'address': destinationAddress,
+      'value': 3,
+      'message': messageTrytes
+  }]
+  const depth = 4
+  const minWeightMagnitude = 14
+  console.log('sendTransfer', messageToSend)
+  iotap.sendTransfer(seed, depth, minWeightMagnitude, transfer).then(bundle => {
+    // console.log(bundle)
+    console.log(`Transaction https://tangle.tips/tx/${bundle[0].hash}`)
+  })
+}
+
+
+function monitorTransactionMessages() {
+  // console.log('monitorTransactionMessages')
+
+  const messageTrytes = 'ODGABDPCADTCGADBGAOBFDXCRCGAQAGAADTCGDGDPCVCTCGADBGAPBXCFDGDHDEARCCDBDHDPCRCHDGAQD99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999'
+
+  let bundle = [{signatureMessageFragment: messageTrytes}]
+  const messageStringified = iota.utils.extractJson(bundle)
+
+  console.log(messageStringified)
+}
+
+
+// getNodeInfo(); setInterval(getNodeInfo, 5 * 1000);
+// getInputs()
+getAccountData(); // setInterval(getAccountData, 5 * 1000);
 // getNewAddress()
-// getBalances()
+
+// sendTransfer()
+// monitorTransactionMessages(); // setInterval(monitorTransactionMessages, 5 * 1000);
