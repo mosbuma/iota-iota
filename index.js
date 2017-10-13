1cf51b516f07879d9448a0a32c386f75308e2193
Eric van Riet Paap <ericvrp@gmail.com>
2017-10-13 15:45:19 +0200

    Ahhh found the problem. No idea why this was working before. But (at least) getInputs doesn't need to run iota.utils.toTrytes(...)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const rp    = require('request-promise') // https://github.com/request/request-promise
 const IOTA  = require('iota.lib.js')     // https://github.com/iotaledger/iota.lib.js
-const IOTAP = require('iotap')           // https://github.com/jimthedev/iotap
 
+// const rp    = require('request-promise') // https://github.com/request/request-promise
+//
 // function findIOTAnode() {
 //   let latestMilestoneIndex = 0
 //   let bestIOTAnode = undefined
@@ -16,8 +16,8 @@ const IOTAP = require('iotap')           // https://github.com/jimthedev/iotap
 //       host = `${protocol}://${hostname}`
 //       // console.log(host, port)
 //       const _iota  = new IOTA({host: host, port: port})
-//       const _iotap = IOTAP.create(_iota) // promisified iota library
-//       _iotap.getNodeInfo().then(info => {
+//       _iota.api.getNodeInfo(function(e, info) {
+//         if (e) throw e;
 //         if (info.latestMilestoneIndex == 227885) return
 //         console.log(info.latestMilestoneIndex, info.latestMilestone)
 //       })
@@ -25,39 +25,39 @@ const IOTAP = require('iotap')           // https://github.com/jimthedev/iotap
 //   })
 // }
 
-// const host = 'http://iota.bitfinex.com'; port = 80;
+// see: http://iotasupport.com/lightwallet.shtml
 const host = 'http://node02.iotatoken.nl'; port = 14265;
-// const host = 'http://localhost';           port = 15000;
 const iota  = new IOTA({ host: host, port: port })
-// const iota  = new IOTA({ host: 'http://node02.iotatoken.nl', port: 14265 })
-const iotap = IOTAP.create(iota) // promisified iota library
 
 
 //
+// const seed = iota.utils.toTrytes('BQIYTZINTTX9ZDVMUHI9Z9SOPQSDPSQQPPBGMELFGSXFVLPJRNZDONYGPCS9BTQBRPKEBWETJLRANJAKL')
 const seed = 'BQIYTZINTTX9ZDVMUHI9Z9SOPQSDPSQQPPBGMELFGSXFVLPJRNZDONYGPCS9BTQBRPKEBWETJLRANJAKL'
-const seedEncoded = iota.utils.toTrytes(seed)
 
-const destinationAddress = '99VUCXYVWSWHKMH9SUAOGN9TXWLOAKGCTSJHXM9WLJGN9HOXEKITUZFAVZJEDYWAGOATJSIHPHKSCIOXBGOGU9CPKX'
+// const destinationAddress = '99VUCXYVWSWHKMH9SUAOGN9TXWLOAKGCTSJHXM9WLJGN9HOXEKITUZFAVZJEDYWAGOATJSIHPHKSCIOXBGOGU9CPKX'
+const destinationAddress = 'AHWFSYWK9ECAPUUACJZYVFXZLLXJFAZBAQRUPNOSHFRQWYMDTH9LZMDIEAZYAM9BWCATLAQSSQJWUIMOBHOIXXQVYZ'
 
 // const securitylevel = 2; // 2 is default
-const nAddresses = 25;
+const nAddresses = 10;
 
 
 function getAccountData() {
   console.log('> getAccountData')
-  const options = {
-    start: 0,
-    end: nAddresses,
-    // security: securitylevel,
-    threshold: 0
-  }
-  iotap.getAccountData(seedEncoded, options).then(data => {
-  // iotap.getAccountData(seedEncoded).then(data => {
+  // const options = {
+  //   start: 0,
+  //   end: nAddresses,
+  //   // security: securitylevel,
+  //   // threshold: 0
+  // }
+  // iota.getAccountData(seed, options, function(e, data) {
+  iota.api.getAccountData(seed, function(e, data) {
+    if (e) throw e;
     console.log('--------- getAccountData data');
     console.log(JSON.stringify(data, null, 4)); // XXX why is transfers[] always empty?
     // console.log(data.addresses)
 
-    // iotap.getBalances(data.addresses, 100).then(json => {
+    // iota.api.getBalances(data.addresses, 100, function(e, json) {
+    //   if (e) throw e;
     //   console.log('--------- getAccountData -> getBalances json');
     //   console.log(json)
     // })
@@ -65,28 +65,33 @@ function getAccountData() {
 }
 
 
-// function getNewAddress() {
-//   console.log('> getNewAddress')
-//   const options = {
-//     // checksum: false,
-//     total: nAddresses,
-//     // security: securitylevel,
-//     returnAll: true
-//   }
-//   iotap.getNewAddress(seedEncoded, options).then(publicAddresses => {
-//     console.log('--------- getNewAddress publicAddresses');
-//     console.log(publicAddresses);
-//
-//     iotap.getBalances(publicAddresses, 100).then(json => {
-//       console.log('--------- getNewAddress -> getBalances json');
-//       console.log(json)
-//     })
-//   });
-// }
+function getNewAddress() {
+  console.log('> getNewAddress')
+  const options = {
+    // checksum: false,
+    total: nAddresses,
+    // security: securitylevel,
+    returnAll: true
+  }
+  iota.api.getNewAddress(seed, options, function(e, publicAddresses) {
+  // iota.api.getNewAddress(seed, function(e, publicAddresses) {
+    if (e) throw e;
+    console.log('--------- getNewAddress publicAddresses');
+    console.log(publicAddresses);
+
+    // iota.api.getBalances(publicAddresses, 100, function(e, json) {
+    //   if (e) throw e;
+    //   console.log('--------- getNewAddress -> getBalances json');
+    //   console.log(json)
+    // })
+  });
+}
 
 
 function getNodeInfo() {
-  iotap.getNodeInfo().then(data => {
+  console.log('> getNodeInfo')
+  iota.api.getNodeInfo(function(e, data) {
+    if (e) throw e;
     console.log('--------- getNodeInfo data')
     console.log(JSON.stringify(data, null, 4))
   })
@@ -94,8 +99,20 @@ function getNodeInfo() {
 
 
 function getInputs() {
-  iotap.getInputs(seed).then(result => {
-    console.log(result)
+  console.log('> getInputs')
+  iota.api.getInputs(seed, function(e, result) {
+    if (e) throw e;
+    console.log('--------- getInputs result', result)
+    // console.log(result.totalBalance)
+  })
+}
+
+
+function getTransfers() {
+  console.log('> getTransfers')
+  iota.api.getTransfers(seed, function(e, result) {
+    if (e) throw e;
+    console.log('--------- getTransfers result', result)
     // console.log(result.totalBalance)
   })
 }
@@ -103,7 +120,7 @@ function getInputs() {
 
 function sendTransfer() {
   const messageToSend = {
-      'id': 'JHKSDIUFSDFKJSDFUI',
+      'id': '<id>',
       'message': new Date().toString()
   }
   const messageStringified = JSON.stringify(messageToSend)
@@ -119,7 +136,8 @@ function sendTransfer() {
   const depth = 4
   const minWeightMagnitude = 14
   console.log('sendTransfer', messageToSend)
-  iotap.sendTransfer(seed, depth, minWeightMagnitude, transfer).then(bundle => {
+  iota.api.sendTransfer(seed, depth, minWeightMagnitude, transfer, function(e, bundle) {
+    if (e) throw e;
     // console.log(bundle)
     console.log(`Transaction https://tangle.tips/tx/${bundle[0].hash}`)
   })
@@ -138,10 +156,12 @@ function monitorTransactionMessages() {
 }
 
 
-// getNodeInfo(); setInterval(getNodeInfo, 5 * 1000);
-// getInputs()
-getAccountData(); // setInterval(getAccountData, 5 * 1000);
+// getNodeInfo(); // setInterval(getNodeInfo, 30 * 1000);
 // getNewAddress()
+
+getInputs()
+// getTransfers()
+// setTimeout(getAccountData, 60*1000); // setInterval(getAccountData, 30 * 1000);
 
 // sendTransfer()
 // monitorTransactionMessages(); // setInterval(monitorTransactionMessages, 5 * 1000);
